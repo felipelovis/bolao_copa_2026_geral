c4188e8e20b52a02e11e22bd7cca77e201fdad12
felipelovis <vasconceloslovison@gmail.com>
2025-11-14 14:11:54 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// ==================== CONFIGURAÇÕES ====================
+
+// DATAS LIMITE por fase (CORRIGIDO PARA 2026!)
+const DATAS_LIMITE = {
+    'Grupo': new Date('2026-06-12T14:00:00'),
+    '16 avos': new Date('2026-06-12T12:00:00'),
+    'Oitavas de final': new Date('2025-11-12T12:00:00'),
+    'Quartas de final': new Date('2025-11-12T16:00:00'),
+    'Semifinais': new Date('2025-11-12T16:00:00'),
+    'Terceiro e Quarto': new Date('2025-11-12T14:00:00'),
+    'Final': new Date('2025-11-12T16:00:00'),
+};
+
+// ID DO GOOGLE SHEETS
+const SPREADSHEET_ID = '1MjSAQUet51WEfdYbToNn7rJ2faK8t2XQsVlFMX9KSQU';
+
+// CHAVE DA API DO GOOGLE
+const API_KEY = 'AIzaSyB2SvzIv9lfSfZ3pX8ukjdFDAPL_KOjsCQ';
+
+// ========================================================
